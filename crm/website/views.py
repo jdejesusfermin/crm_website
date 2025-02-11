ac0ff12a5a0defbf5a309bc99d5d912663fbbca2
J Fermin <149550908+jdejesusfermin@users.noreply.github.com>
2025-02-11 11:04:23 +0800

    Add files via upload

---
+++ crm/website/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
+
+def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You're now logged in!")
+            return redirect('home')
+        else:
+            messages.success(request, "Please try again")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "bye-bye!")
+    return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Registration successful!")
+            return redirect('home')
+    return render(request, 'register.html', {"form": form})
