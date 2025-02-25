ef4190c13423016d5901c280c960c99d5959eeaf
J Fermin <149550908+jdejesusfermin@users.noreply.github.com>
2025-02-25 10:56:24 +0800

    Add files via upload

--- crm/website/urls.py
+++ crm/website/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('login/', views.login_user, name='login')
+    path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
-    path('register/', views.register_user, name='register')
+    path('register/', views.register_user, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('record/<int:pk>/', views.customer_record, name='record'),
 ]
--- crm/website/views.py
+++ crm/website/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
+    
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -14,10 +18,25 @@ def home(request):
             messages.success(request, "You're now logged in!")
             return redirect('home')
         else:
-            messages.success(request, "Please try again")
+            messages.error(request, "Invalid username or password. Please try again.")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {"records": records})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You're now logged in!")
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
 
 def logout_user(request):
     logout(request)
@@ -33,3 +52,28 @@ def register_user(request):
             messages.success(request, "Registration successful!")
             return redirect('home')
     return render(request, 'register.html', {"form": form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)  
+        return render(request, 'record.html', {'customer_record': customer_record})  
+    else:
+        messages.success(request, "You must be logged in to view the page")
+        return redirect('home')
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record updated successfully")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form, 'current_record': current_record})
+    else:
+        messages.success(request, "You must be logged in to update records")
+        return redirect('home')
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
